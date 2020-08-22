2900af774d83dc3a167408efa8c33d246474fd71
Ben Daniels <benbanj@hotmail.com>
2020-08-22 14:03:07 +0100

    Adding UserStock resources

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,8 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
 
   def self.look_up(ticker_symbol)
     client = IEX::Api::Client.new(
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'stocks/search'
   root 'welcome#index'
+  resources :user_stocks
+  get 'stocks/search'
   devise_for :users
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
